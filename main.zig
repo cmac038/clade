155bfff933f448c13fbf8f8662463e6a1437ddcd
Chris M <cmac038@gmail.com>
2024-09-27 01:28:25 -0400

    Refactored to use custom BigDate struct
    BigDate handles date incrementing, encoding, and printing
    Also added more tests

--- main.zig
+++ main.zig
@@ -1,12 +1,70 @@
-// This program outputs dates which add up to a target number.
-// start_year and end_year delineate the date range.
-// Totals for each year are provided as output.
+//! This program outputs dates which add up to a target number.
+//! start_year and end_year delineate the date range.
+//! Totals for each year are provided as output.
 // TODO: file output options for visualization purposes?
 // TODO: better formatting for output; horizontal, buffered output?
 
 const std = @import("std");
 const stdout = std.io.getStdOut().writer();
 const testing = std.testing;
+
+const BigDate = struct {
+    year: u32,
+    month: u32,
+    day: u32,
+
+    /// Increment by one day, handling month and year turnovers
+    /// Return true if incrementing results in a year turnover
+    pub fn increment(this: *BigDate) bool {
+        this.day += 1;
+        // check for turnover
+        switch (this.month) {
+            // 31 day months
+            1, 3, 5, 7, 8, 10 => {
+                if (this.day > 31) {
+                    this.month += 1;
+                    this.day = 1;
+                }
+            },
+            // 30 day months
+            4, 6, 9, 11 => {
+                if (this.day > 30) {
+                    this.month += 1;
+                    this.day = 1;
+                }
+            },
+            // February
+            2 => {
+                if (this.day > 28) {
+                    this.month += 1;
+                    this.day = 1;
+                }
+            },
+            // December (year turnover)
+            12 => {
+                if (this.day > 31) {
+                    this.year += 1;
+                    this.month = 1;
+                    this.day = 1;
+                    return true;
+                }
+            },
+            else => unreachable,
+        }
+        return false;
+    }
+
+    /// Returns the date as a u32 encoded as yyyymmdd
+    /// For example, 05/26/1987 -> 19870526
+    pub inline fn bigDate(self: BigDate) u32 {
+        return (self.year * 10000) + (self.month * 100) + self.day;
+    }
+
+    /// Print to stdout in the format mm/dd/yyyy
+    pub inline fn print(self: BigDate) !void {
+        try stdout.print("| {:0>2}/{:0>2}/{} |\n", .{ self.month, self.day, self.year });
+    }
+};
 
 pub fn main() !void {
     // user inputs
@@ -20,47 +78,24 @@ pub fn main() !void {
 
     var count: u32 = 0;
     var total: u32 = 0;
-    var year = start_year;
-    var month: u32 = 1;
-    var day: u32 = 1;
-    var date = convertToBigDate(year, month, day);
+
+    var date = BigDate{
+        .year = start_year,
+        .month = 1,
+        .day = 1,
+    };
+
     try stdout.print(
         \\|============|
         \\|    {}    |
         \\|============|
         \\
-    , .{year});
+    , .{date.year});
 
-    while (year < end_year) : (date = convertToBigDate(year, month, day)) {
-        // handle different month lengths
-        switch (month) {
-            1, 3, 5, 7, 8, 10, 12 => {
-                if (day > 31) {
-                    month += 1;
-                    day = 1;
-                }
-            },
-            4, 6, 9, 11 => {
-                if (day > 30) {
-                    month += 1;
-                    day = 1;
-                }
-            },
-            2 => {
-                if (day > 28) {
-                    month += 1;
-                    day = 1;
-                }
-            },
-            else => unreachable,
-        }
-
-        // handle year turnover
-        if (month > 12) {
-            year += 1;
-            month = 1;
-            day = 1;
-            if (year == end_year) {
+    while (true) {
+        const is_new_year = date.increment();
+        if (is_new_year) {
+            if (date.year == end_year) {
                 // don't print year if it's the last iteration
                 try stdout.print(
                     \\|------------|
@@ -68,7 +103,8 @@ pub fn main() !void {
                     \\|============|
                     \\
                 , .{count});
-                continue;
+                total += count;
+                break;
             }
             try stdout.print(
                 \\|------------|
@@ -77,25 +113,21 @@ pub fn main() !void {
                 \\|    {}    |
                 \\|============|
                 \\
-            , .{ count, year });
+            , .{ count, date.year });
             total += count;
             count = 0;
-            continue;
         }
 
-        // date will always be a valid date at this point
-        if (pythagorizeIterative(date, num_digits) == target) {
-            try stdout.print("| {:0>2}/{:0>2}/{} |\n", .{ month, day, year });
+        if (pythagorizeIterative(date.bigDate(), num_digits) == target) {
+            try date.print();
             count += 1;
         }
-
-        day += 1;
     }
 
     try stdout.print("\nGrand Total: {}\n", .{total});
 }
 
-// Pythagorization means adding up all the individual digits.
+// Pythagorization means adding up all the individual digits
 // For example, 05/26/1987 -> 5 + 2 + 6 + 1 + 9 + 8 + 7 = 38
 //
 // recursive version
@@ -124,45 +156,126 @@ fn pythagorizeIterative(input: u32, num_digits: u32) u32 {
     return total;
 }
 
-// BigDate = date in the format yyyymmdd.
-// For example, 05/26/1987 -> 19870526
-inline fn convertToBigDate(year: u32, month: u32, day: u32) u32 {
-    return (year * 10000) + (month * 100) + day;
-}
-
 // TESTING
+// pythagorizeRecursive
 test "pythagorizeRecursive 1 digit" {
     try testing.expect(pythagorizeRecursive(9, 1) == 9);
 }
-
 test "pythagorizeRecursive 2 digit" {
     try testing.expect(pythagorizeRecursive(15, 2) == 6);
 }
-
 test "pythagorizeRecursive 3 digit" {
     try testing.expect(pythagorizeRecursive(286, 3) == 16);
 }
-
 test "pythagorizeRecursive 4 digit" {
     try testing.expect(pythagorizeRecursive(1996, 4) == 25);
 }
+test "pythagorizeRecursive 8 digit" {
+    try testing.expect(pythagorizeRecursive(19870526, 8) == 38);
+}
 
+// pythagorizeIterative
 test "pythagorizeIterative 1 digit" {
     try testing.expect(pythagorizeIterative(9, 1) == 9);
 }
-
 test "pythagorizeIterative 2 digit" {
     try testing.expect(pythagorizeIterative(15, 2) == 6);
 }
-
 test "pythagorizeIterative 3 digit" {
     try testing.expect(pythagorizeIterative(286, 3) == 16);
 }
-
 test "pythagorizeIterative 4 digit" {
     try testing.expect(pythagorizeIterative(1996, 4) == 25);
 }
-
 test "pythagorizeIterative 8 digit" {
     try testing.expect(pythagorizeIterative(19870526, 8) == 38);
+}
+
+// BigDate increment
+test "BigDate increment day" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 11,
+        .day = 9,
+    };
+    _ = date.increment();
+    try testing.expect(date.year == 1950 and date.month == 11 and date.day == 10);
+}
+test "BigDate increment 30 day month" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 11,
+        .day = 30,
+    };
+    _ = date.increment();
+    try testing.expect(date.year == 1950 and date.month == 12 and date.day == 1);
+}
+test "BigDate increment 31 day month" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 8,
+        .day = 31,
+    };
+    _ = date.increment();
+    try testing.expect(date.year == 1950 and date.month == 9 and date.day == 1);
+}
+test "BigDate increment February" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 2,
+        .day = 28,
+    };
+    _ = date.increment();
+    try testing.expect(date.year == 1950 and date.month == 3 and date.day == 1);
+}
+test "BigDate increment year" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 12,
+        .day = 31,
+    };
+    _ = date.increment();
+    try testing.expect(date.year == 1951 and date.month == 1 and date.day == 1);
+}
+
+// BigDate bigDate
+test "BigDate bigDate 1" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 11,
+        .day = 9,
+    };
+    try testing.expect(date.bigDate() == 19501109);
+}
+test "BigDate bigDate 2" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 5,
+        .day = 9,
+    };
+    try testing.expect(date.bigDate() == 19500509);
+}
+test "BigDate bigDate 3" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 11,
+        .day = 11,
+    };
+    try testing.expect(date.bigDate() == 19501111);
+}
+test "BigDate bigDate 4" {
+    var date = BigDate{
+        .year = 1950,
+        .month = 5,
+        .day = 19,
+    };
+    try testing.expect(date.bigDate() == 19500519);
+}
+test "BigDate bigDate 5" {
+    var date = BigDate{
+        .year = 2000,
+        .month = 12,
+        .day = 19,
+    };
+    try testing.expect(date.bigDate() == 20001219);
 }
