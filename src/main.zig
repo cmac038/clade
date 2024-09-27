6878d7055ce8c277e3ac3583ce25598754572035
Chris M <cmac038@gmail.com>
2024-09-27 12:10:17 -0400

    Added leap year support and modified algorithm
    Now sums date digits by year, month, and date instead of combining
    Made sumDigits a method if Date (renamed from BigDate)

--- src/main.zig
+++ src/main.zig
@@ -8,14 +8,15 @@ const std = @import("std");
 const stdout_file = std.io.getStdOut().writer();
 const testing = std.testing;
 
-const BigDate = struct {
+const Date = struct {
     year: u32,
     month: u32,
     day: u32,
 
     /// Increment by one day, handling month and year turnovers
+    /// Also handles leap years
     /// Return true if incrementing results in a year turnover
-    pub inline fn increment(this: *BigDate) bool {
+    pub inline fn increment(this: *Date) bool {
         this.day += 1;
         // check for turnover
         switch (this.month) {
@@ -35,6 +36,7 @@ const BigDate = struct {
             },
             // February
             2 => {
+                if (this.day == 29 and this.isLeapYear()) return false;
                 if (this.day > 28) {
                     this.month += 1;
                     this.day = 1;
@@ -54,29 +56,37 @@ const BigDate = struct {
         return false;
     }
 
-    /// Returns the date as a u32 encoded as yyyymmdd
-    /// For example, 05/26/1987 -> 19870526
-    pub inline fn bigDate(self: BigDate) u32 {
-        return (self.year * 10000) + (self.month * 100) + self.day;
+    /// Leap year rules:
+    /// divisible by 4 == true
+    /// divisible by 100 == false EXCEPT when divisible by 400
+    pub inline fn isLeapYear(self: Date) bool {
+        if (self.year % 100 == 0) {
+            if (self.year % 400 == 0) return true;
+            return false;
+        }
+        return self.year % 4 == 0;
+    }
+
+    /// Sum up all the digits in the date
+    /// e.g. 06/27/1998 -> 6 + 2 + 7 + 1 + 9 + 9 + 8
+    pub inline fn sumDigits(self: Date) u32 {
+        return sumDigitsRecursive(self.year, 10000) + sumDigitsRecursive(self.month, 10) + sumDigitsRecursive(self.day, 10);
     }
 
     /// Print to writer in the format mm/dd/yyyy
-    pub inline fn print(self: BigDate, writer: anytype) !void {
+    pub inline fn print(self: Date, writer: anytype) !void {
         try writer.print("| {:0>2}/{:0>2}/{} |\n", .{ self.month, self.day, self.year });
     }
 };
 
-// Pythagorization means adding up all the individual digits
-// For example, 05/26/1987 -> 5 + 2 + 6 + 1 + 9 + 8 + 7 = 38
-//
-inline fn pythagorizeRecursive(number: u32, divisor: u32) u32 {
+inline fn sumDigitsRecursive(number: u32, divisor: u32) u32 {
     if (divisor == 1) {
         return number;
     }
-    return (number / divisor) + pythagorizeRecursive(number % divisor, divisor / 10);
+    return (number / divisor) + sumDigitsRecursive(number % divisor, divisor / 10);
 }
 
-inline fn pythagorizeIterative(input: u32, initial_divisor: u32) u32 {
+inline fn sumDigitsIterative(input: u32, initial_divisor: u32) u32 {
     var total: u32 = 0;
     var number = input;
     var divisor = initial_divisor;
@@ -91,9 +101,8 @@ pub fn main() !void {
     // user inputs
     // TODO: turn these into commandline args?
     const target: u32 = 36;
-    const start_year: u32 = 1900;
-    const end_year: u32 = 2025;
-    const initial_divisor: u32 = 1e7;
+    const start_year: u32 = 0;
+    const end_year: u32 = 100000;
 
     // buffered writer for better performance
     var buf = std.io.bufferedWriter(stdout_file);
@@ -103,7 +112,7 @@ pub fn main() !void {
     var count: u32 = 0;
     var total: u32 = 0;
 
-    var date = BigDate{
+    var date = Date{
         .year = start_year,
         .month = 1,
         .day = 1,
@@ -144,7 +153,7 @@ pub fn main() !void {
             count = 0;
         }
 
-        if (pythagorizeRecursive(date.bigDate(), initial_divisor) == target) {
+        if (date.sumDigits() == target) {
             try date.print(stdout);
             count += 1;
         }
@@ -155,53 +164,61 @@ pub fn main() !void {
 }
 
 // TESTING
-// pythagorizeRecursive
-test "pythagorizeRecursive 1 digit" {
-    try testing.expect(pythagorizeRecursive(9, 1) == 9);
+// sumDigitsRecursive
+test "sumDigitsRecursive 1 digit" {
+    try testing.expect(sumDigitsRecursive(9, 1) == 9);
 }
-test "pythagorizeRecursive 2 digit" {
-    try testing.expect(pythagorizeRecursive(15, 10) == 6);
+test "sumDigitsRecursive 2 digit" {
+    try testing.expect(sumDigitsRecursive(15, 10) == 6);
 }
-test "pythagorizeRecursive 3 digit" {
-    try testing.expect(pythagorizeRecursive(286, 100) == 16);
+test "sumDigitsRecursive 3 digit" {
+    try testing.expect(sumDigitsRecursive(286, 100) == 16);
 }
-test "pythagorizeRecursive 4 digit" {
-    try testing.expect(pythagorizeRecursive(1996, 1000) == 25);
+test "sumDigitsRecursive 4 digit" {
+    try testing.expect(sumDigitsRecursive(1996, 1000) == 25);
 }
-test "pythagorizeRecursive 8 digit" {
-    try testing.expect(pythagorizeRecursive(19870526, 1e7) == 38);
-}
-
-// pythagorizeIterative
-test "pythagorizeIterative 1 digit" {
-    const result = pythagorizeIterative(9, 1);
-    std.debug.print("pythagorizeIterative 1 digit: {}\n", .{result});
-    try testing.expect(result == 9);
-}
-test "pythagorizeIterative 2 digit" {
-    const result = pythagorizeIterative(15, 10);
-    std.debug.print("pythagorizeIterative 2 digit: {}\n", .{result});
-    try testing.expect(result == 6);
-}
-test "pythagorizeIterative 3 digit" {
-    const result = pythagorizeIterative(286, 100);
-    std.debug.print("pythagorizeIterative 3 digit: {}\n", .{result});
-    try testing.expect(result == 16);
-}
-test "pythagorizeIterative 4 digit" {
-    const result = pythagorizeIterative(1996, 1000);
-    std.debug.print("pythagorizeIterative 4 digit: {}\n", .{result});
-    try testing.expect(result == 25);
-}
-test "pythagorizeIterative 8 digit" {
-    const result = pythagorizeIterative(19870526, 1e7);
-    std.debug.print("pythagorizeIterative 8 digit: {}\n", .{result});
-    try testing.expect(result == 38);
+test "sumDigitsRecursive 8 digit" {
+    try testing.expect(sumDigitsRecursive(19870526, 1e7) == 38);
 }
 
-// BigDate increment
-test "BigDate increment day" {
-    var date = BigDate{
+// sumDigitsIterative
+test "sumDigitsIterative 1 digit" {
+    try testing.expect(sumDigitsIterative(9, 1) == 9);
+}
+test "sumDigitsIterative 2 digit" {
+    try testing.expect(sumDigitsIterative(15, 10) == 6);
+}
+test "sumDigitsIterative 3 digit" {
+    try testing.expect(sumDigitsIterative(286, 100) == 16);
+}
+test "sumDigitsIterative 4 digit" {
+    try testing.expect(sumDigitsIterative(1996, 1000) == 25);
+}
+test "sumDigitsIterative 8 digit" {
+    try testing.expect(sumDigitsIterative(19870526, 1e7) == 38);
+}
+
+// Date isLeapYear
+test "Date isLeapYear divisible by 4 (true)" {
+    var date = Date{ .year = 2024, .month = 8, .day = 5 };
+    try testing.expect(date.isLeapYear());
+}
+test "Date isLeapYear divisible by 400 (true)" {
+    var date = Date{ .year = 2000, .month = 8, .day = 5 };
+    try testing.expect(date.isLeapYear());
+}
+test "Date isLeapYear divisible by 100 (false)" {
+    var date = Date{ .year = 2100, .month = 8, .day = 5 };
+    try testing.expect(!date.isLeapYear());
+}
+test "Date isLeapYear (false)" {
+    var date = Date{ .year = 2022, .month = 8, .day = 5 };
+    try testing.expect(!date.isLeapYear());
+}
+
+// Date increment
+test "Date increment day" {
+    var date = Date{
         .year = 1950,
         .month = 11,
         .day = 9,
@@ -209,8 +226,8 @@ test "BigDate increment day" {
     _ = date.increment();
     try testing.expect(date.year == 1950 and date.month == 11 and date.day == 10);
 }
-test "BigDate increment 30 day month" {
-    var date = BigDate{
+test "Date increment 30 day month" {
+    var date = Date{
         .year = 1950,
         .month = 11,
         .day = 30,
@@ -218,8 +235,8 @@ test "BigDate increment 30 day month" {
     _ = date.increment();
     try testing.expect(date.year == 1950 and date.month == 12 and date.day == 1);
 }
-test "BigDate increment 31 day month" {
-    var date = BigDate{
+test "Date increment 31 day month" {
+    var date = Date{
         .year = 1950,
         .month = 8,
         .day = 31,
@@ -227,8 +244,8 @@ test "BigDate increment 31 day month" {
     _ = date.increment();
     try testing.expect(date.year == 1950 and date.month == 9 and date.day == 1);
 }
-test "BigDate increment February" {
-    var date = BigDate{
+test "Date increment February (not Leap Year)" {
+    var date = Date{
         .year = 1950,
         .month = 2,
         .day = 28,
@@ -236,8 +253,17 @@ test "BigDate increment February" {
     _ = date.increment();
     try testing.expect(date.year == 1950 and date.month == 3 and date.day == 1);
 }
-test "BigDate increment year" {
-    var date = BigDate{
+test "Date increment February (Leap Year)" {
+    var date = Date{
+        .year = 2024,
+        .month = 2,
+        .day = 28,
+    };
+    _ = date.increment();
+    try testing.expect(date.year == 2024 and date.month == 2 and date.day == 29);
+}
+test "Date increment year" {
+    var date = Date{
         .year = 1950,
         .month = 12,
         .day = 31,
@@ -246,44 +272,20 @@ test "BigDate increment year" {
     try testing.expect(date.year == 1951 and date.month == 1 and date.day == 1);
 }
 
-// BigDate bigDate
-test "BigDate bigDate 1" {
-    var date = BigDate{
+// Date sumDigits
+test "Date sumDigits 12/31/1950" {
+    var date = Date{
         .year = 1950,
-        .month = 11,
-        .day = 9,
-    };
-    try testing.expect(date.bigDate() == 19501109);
-}
-test "BigDate bigDate 2" {
-    var date = BigDate{
-        .year = 1950,
-        .month = 5,
-        .day = 9,
-    };
-    try testing.expect(date.bigDate() == 19500509);
-}
-test "BigDate bigDate 3" {
-    var date = BigDate{
-        .year = 1950,
-        .month = 11,
-        .day = 11,
-    };
-    try testing.expect(date.bigDate() == 19501111);
-}
-test "BigDate bigDate 4" {
-    var date = BigDate{
-        .year = 1950,
-        .month = 5,
-        .day = 19,
-    };
-    try testing.expect(date.bigDate() == 19500519);
-}
-test "BigDate bigDate 5" {
-    var date = BigDate{
-        .year = 2000,
         .month = 12,
-        .day = 19,
+        .day = 31,
     };
-    try testing.expect(date.bigDate() == 20001219);
+    try testing.expect(date.sumDigits() == 22);
+}
+test "Date sumDigits 08/21/1996" {
+    var date = Date{
+        .year = 1996,
+        .month = 8,
+        .day = 21,
+    };
+    try testing.expect(date.sumDigits() == 36);
 }
